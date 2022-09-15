d807b2d03926b80061ddc6799baea89774977ccd
Reno Muijsenberg <490412@student.fontys.nl>
2022-09-15 13:16:16 +0200

    first commit

---
+++ src/test/java/com/mdma/accountservice/AccountServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mdma.accountservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
